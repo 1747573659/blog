9b7e222322b6d34c2638f7d5c6d074590d56f279
chensheng <chensheng@kemai.cn>
2021-09-06 20:30:14 +0800

    提交代码01:25:45

--- src/dsReactRedux/dsReactRedux.js
+++ src/dsReactRedux/dsReactRedux.js
@@ -1,3 +1,33 @@
+/**
+ * 
+ * 跨层级数据传递
+ * context三步走
+ * step1：创建一个context对象
+ */
+import React, {useContext} from 'react'
+const Context = React.createContext()
+
+
+// Provider传递store
+export function Provider ({store, children}) {
+  return <Context.Provider value={store}>children</Context.Provider>
+}
+
+// 子孙组件接收store
+//1) Consumer
+//2) contextType 只能用在类组件，只能订阅单一的context来源
+//3) useContext 只能用在函数组件或者是自定义hook
+export const connect = (mapStateToProps, mapDispatchToProps) => WrappedComponent => props => {
+
+  const store = useContext(Context)
+
+  const stateProps = mapStateToProps(store.getState())
+  const dispatchProps = mapDispatchToProps
+
+
+  return <WrappedComponent {...props} {...stateProps} {...dispatchProps}/>
+}
+
 function bindActionCreator(creator, dispatch) {
   return (...args) => dispatch(creator(...args))
 }
