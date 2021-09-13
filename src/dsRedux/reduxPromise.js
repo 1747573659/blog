6242baf57804d9f4414494788ca240a33714df92
chensheng <chensheng@kemai.cn>
2021-09-13 15:56:58 +0800

    提交代码

--- src/dsRedux/reduxPromise.js
+++
@@ -1,8 +0,0 @@
-const reduxPromise =
-  ({ getState, dispatch }) =>
-  next =>
-  action => {
-    action instanceof Promise ? action().then(dispatch) : next(action)
-  }
-
-export default reduxPromise
